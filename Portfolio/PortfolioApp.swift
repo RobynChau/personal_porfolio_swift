3b37ba428f3ef6207bf110718092e9b9f2cc15b8
Robyn Chau <chauhuuphat21062001@gmail.com>
2022-03-17 15:41:27 +0700

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Robyn Chau on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
