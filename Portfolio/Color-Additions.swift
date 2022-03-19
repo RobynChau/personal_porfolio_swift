1f3880010dc87fb80a0a86f6f5c577cb68944c6a
Châu Hữu Phát <chauhuuphat21062001@gmail.com>
2022-03-20 00:51:29 +0700

    Reading Awards JSON + Create Home View

---
+++ Portfolio/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  Portfolio
+//
+//  Created by Robyn Chau on 20/03/2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
